df86e2829a1366bb71705de1e81528789285f806
Minhaj Ahmed Moin <31467353+Mynhaj@users.noreply.github.com>
2018-09-22 13:27:04 +0500

    Debugged

--- sherlock.py
+++ sherlock.py
@@ -3,8 +3,12 @@ def sherlock(a , b):
 def encrypt(s):
     p = "".join(s.split(" "));
     rows = columns = int(len(p)**0.5)
-    if (len(p)**0.5 - rows)>0:
-        rows += 1;
+    if (len(p)**0.5 - rows)>0: rows += 1;
+    if (len(p)**0.5 - columns)>0.5: columns += 1;
+    print (columns, rows)
     k = [p[i:rows+i] for i in range(0,len(p),rows)]
+    for i in k: print (i)
     p = [[k[i][j] for i in range(columns) if j < len(k[i])] for j in range(rows)]
     for i in p: print(''.join(i),end=' ')
+
+
